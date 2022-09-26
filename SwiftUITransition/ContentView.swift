c9f7ae82141766584ffd2407c806d43277d454c8
mertkanak <107624227+mertkanak@users.noreply.github.com>
2022-09-26 13:45:43 +0300

    Initial commit

---
+++ SwiftUITransition/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  SwiftUITransition
+//
+//  Created by mert Kanak on 26.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var show = false
+    
+    var body: some View {
+        VStack {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 300, height: 300)
+                .foregroundColor(.green)
+                .overlay(
+                    Text("Show details")
+                        .font(.system(.largeTitle, design: .rounded))
+                        .bold()
+                        .foregroundColor(.white)
+                )
+            
+            if show {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 300, height: 300)
+                .foregroundColor(.purple)
+                .overlay(
+                    Text("Well, here is the details")
+                        .font(.system(.largeTitle, design: .rounded))
+                        .bold()
+                        .foregroundColor(.white)
+                )
+            }
+        }
+        .onTapGesture {
+            withAnimation(Animation.spring()) {
+                self.show.toggle()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUITransition/SwiftUITransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITransitionApp.swift
+//  SwiftUITransition
+//
+//  Created by mert Kanak on 26.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
